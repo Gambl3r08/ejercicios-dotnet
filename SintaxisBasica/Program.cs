c8d46113e9c356c4e063720673ffcd8e6a9d52ec
Roberto Lozada <robertojoselozada@gmail.com>
2021-06-16 11:16:09 -0500

    uso de metodos y sintaxis básica

---
+++ SintaxisBasica/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SintaxisBasica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var num = 20;
+            //int num2 = 50;
+            //Console.WriteLine("Asignación automatica: "+num);
+            //Console.WriteLine("Asignación de tipo explicita: "+num2);
+
+            Console.WriteLine("Ingrese un valor numerico");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num >= 20){
+                Console.WriteLine("Num mayor o igual a  20");
+            } else if(num == 0){
+                Console.WriteLine("Num igual a 0 ");
+            } else if(num >= 20){
+                Console.WriteLine("Num menor que 20");
+            }
+
+            Console.WriteLine("Num : "+ num);
+
+
+
+
+        }
+    }
+}
---
+++ usoMetodos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace usoMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 50;
+            Console.WriteLine(suma (a, b));
+            
+        }
+
+        static int suma(int a, int b){
+            var result  = a + b;
+            return result;
+        }
+
+
+
+
+    }
+}
+
+
